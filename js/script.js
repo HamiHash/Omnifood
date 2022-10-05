336c022847845e5eb1bb1d52a688b60989e39593
HamiHash <hami.hash@yahoo.com>
2022-10-05 15:13:30 +0330

    Links
    Added smooth scrolling animation for all the browsers including safari and edge,
    and destination for all the links and anchor elements.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+//   Make mobile nav work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const haederEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  haederEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // scroll to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // scroll to other link
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation after clicking on the links
+    if (link.classList.contains("main-nav-link"))
+      haederEl.classList.toggle("nav-open");
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
